18e9397df8d8709f776c647f26a51934bebaafb4
Naman <98084335+namangarg2002@users.noreply.github.com>
2025-02-08 23:07:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -4,38 +4,37 @@ using namespace std;
 #define MAX_HEIGHT 100000
 
 // Tree Node
-struct Node
-{
+class Node {
+  public:
     int data;
     Node* left;
     Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
 };
 
 // Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-
-    return temp;
+Node* newNode(int val) {
+    return new Node(val);
 }
 
-
 // Function to Build Tree
-Node* buildTree(string str)
-{
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
+    if (str.length() == 0 || str[0] == 'N')
         return NULL;
 
     // Creating vector of strings from input
-    // string after spliting by space
+    // string after splitting by space
     vector<string> ip;
 
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
 
     // Create the root of the tree
@@ -47,7 +46,7 @@ Node* buildTree(string str)
 
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
+    while (!queue.empty() && i < ip.size()) {
 
         // Get and remove the front of the queue
         Node* currNode = queue.front();
@@ -57,7 +56,7 @@ Node* buildTree(string str)
         string currVal = ip[i];
 
         // If the left child is not null
-        if(currVal != "N") {
+        if (currVal != "N") {
 
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
@@ -68,12 +67,12 @@ Node* buildTree(string str)
 
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
 
         // If the right child is not null
-        if(currVal != "N") {
+        if (currVal != "N") {
 
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
@@ -88,125 +87,94 @@ Node* buildTree(string str)
 }
 
 
-
-
-
-
-
-
-
 // } Driver Code Ends
-/* A binary tree Node
-struct Node
-{
+/*
+// Tree Node
+class Node {
+  public:
     int data;
-    Node* left, * right;
-}; */
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
 
 class Solution {
-private:
-    void printLeftBoundry(Node *root, vector<int> &result)
-    {
-      // base case
-      if (root == NULL)
-        return;
-      // go back when leaf node found
-      if (root->left == NULL && root->right == NULL)
-      {
-        return;
-      }
-      result.push_back(root->data);
-      // if it is possible to move left then go ahead but dont move in both direction i.e., left or right
-      // possible to move left
-      if (root->left != NULL)
-      {
-        printLeftBoundry(root->left, result);
-      }
-      else
-      {
-        // not possible to move left
-        printLeftBoundry(root->right, result);
-      }
+  public:
+    void printLeftBranch(Node* root, vector<int>&ans){
+        if(!root) return;
+        if(!root->left && !root->right) return;
+        
+        ans.push_back(root->data);
+        
+        if(root->left != NULL){
+            printLeftBranch(root->left, ans);
+        }else{
+            printLeftBranch(root->right, ans);
+        }
     }
-    
-    void printLeafBoundry(Node *root,vector<int> &result)
-    {
-      if (root == NULL)
-        return;
-    
-      if (root->left == NULL && root->right == NULL)
-      {
-        result.push_back(root->data);
-      }
-      printLeafBoundry(root->left, result);
-      printLeafBoundry(root->right, result);
+    void printRightBranch(Node* root, vector<int>&ans){
+        if(!root) return;
+        if(!root->left && !root->right) return;
+        
+        if(root->right != NULL){
+            printRightBranch(root->right, ans);
+        }else{
+            printRightBranch(root->left, ans);
+        }
+        ans.push_back(root->data);
     }
-    
-    void printRightBoundry(Node *root, vector<int> &result)
-    {
-      if (root == NULL)
-        return;
-    
-      if (root->left == NULL && root->right == NULL)
-      {
-        return;
-      }
-      if (root->right != NULL)
-      {
-        printRightBoundry(root->right, result);
-      }
-      else
-      {
-        printRightBoundry(root->left, result);
-      }
-      result.push_back(root->data);
+    void printLeafBoundry(Node* root, vector<int>&ans){
+        if(!root) return;
+        if(!root->left && !root->right){
+            ans.push_back(root->data);
+        }
+        printLeafBoundry(root->left, ans);
+        printLeafBoundry(root->right, ans);
     }
-    
-public:
-    vector <int> boundary(Node *root)
-    {
-        vector<int> result;
-        if (root == NULL)
-            return result;
-    
-        // Add the root node to the result
-        result.push_back(root->data);
-    
-        // Print the left boundary excluding the root and leaf nodes
-        printLeftBoundry(root->left, result);
-    
-        // Print all the leaf nodes
-        printLeafBoundry(root->left, result);
-        printLeafBoundry(root->right, result);
-    
-        // Print the right boundary excluding the root and leaf nodes
-        printRightBoundry(root->right, result);
-    
-        return result;
+    vector<int> boundaryTraversal(Node *root) {
+        if(!root) return {};
+        
+        vector<int>ans;
+        ans.push_back(root->data);
+        printLeftBranch(root->left, ans);
+        printLeafBoundry(root->left, ans);
+        printLeafBoundry(root->right, ans);
+        printRightBranch(root->right, ans);
+        
+        return ans;
+        
     }
 };
 
 //{ Driver Code Starts.
 
-/* Driver program to test size function*/
-
+/* Driver program to test size function */
 int main() {
     int t;
     string tc;
     getline(cin, tc);
-    t=stoi(tc);
-    while(t--)
-    {
-        string s ,ch;
+    t = stoi(tc);
+    while (t--) {
+        string s, ch;
         getline(cin, s);
         Node* root = buildTree(s);
         Solution ob;
-        vector <int> res = ob.boundary(root);
-        for (int i : res) cout << i << " ";
+        vector<int> res = ob.boundaryTraversal(root);
+        for (int i : res)
+            cout << i << " ";
         cout << endl;
-    
-cout << "~" << "\n";
-}
+
+        cout << "~"
+             << "\n";
+    }
     return 0;
 }
+
 // } Driver Code Ends
