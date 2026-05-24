272b969b2699d3130627851d1606a0b029cb5ef6
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-05-24 18:44:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++){
+            char ch = s[i];
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.empty()){
+                    return false;
+                }else if(ch == ')' && st.top() == '(' || ch == '}' && st.top() == '{' || ch == ']' && st.top() == '['){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(st.size() == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
