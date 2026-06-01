02a0f13b9bfa9e16257771856fb55990e91f4286
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-06-01 10:44:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+  public:
+    int maxLength(string& s) {
+        int n = s.length();
+        stack<int> st;
+        st.push(-1);
+        int maxLen = 0;
+        for(int i=0; i<n; i++){
+            char ch = s[i];
+            if(ch == '('){
+                st.push(i);
+            }else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }else{
+                    int len = i - st.top();
+                    maxLen = max(len, maxLen);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+};
