4301cbd065491107650f0ef3f912c1d0f1483177
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-09-09 12:32:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of Two/gcd-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        while(b != 0){
+            int c = a % b;
+            a = b;
+            b = c;
+        }
+        
+        return a;
+        
+    }
+};
