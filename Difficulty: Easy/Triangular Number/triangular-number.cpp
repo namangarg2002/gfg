e44068e80efe63e30af36ee49d11c34c8519f888
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-09-09 13:46:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Triangular Number/triangular-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int isTriangular(int n) {
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            sum += i;
+            
+            if(sum == n){
+                return 1;
+            }
+        }
+        
+        return 0;
+        
+    }
+};
