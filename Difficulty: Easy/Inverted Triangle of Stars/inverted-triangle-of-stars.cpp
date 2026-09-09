79780c6854246d0b817ca73cdafffe166c10e5ba
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-09-09 13:24:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inverted Triangle of Stars/inverted-triangle-of-stars.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        // Spaces
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        
+        // stars
+        for(int j=0; j<2*(n-i)-1; j++){
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
