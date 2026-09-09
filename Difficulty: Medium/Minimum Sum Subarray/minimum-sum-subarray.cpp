9744f3dcfa48f85f0b02cb35083b4624555f1725
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-09-09 19:42:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Sum Subarray/minimum-sum-subarray.cpp
+++ Difficulty: Medium/Minimum Sum Subarray/minimum-sum-subarray.cpp
@@ -1,16 +1,17 @@
 class Solution {
   public:
-    int smallestSumSubarray(vector<int>& arr) {
+    int minSubarraySum(vector<int> &arr) {
         int bestEnding = arr[0];
         int ans = arr[0];
+        
         for(int i=1; i<arr.size(); i++){
             int b1 = bestEnding + arr[i];
             int b2 = arr[i];
+            
             bestEnding = min(b1, b2);
             ans = min(ans, bestEnding);
         }
         
         return ans;
-        
     }
 };
