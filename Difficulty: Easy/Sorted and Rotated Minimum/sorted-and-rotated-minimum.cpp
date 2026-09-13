17f7b61eab9b9268208629cc63a1af311276a4c4
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-09-13 20:55:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.cpp
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.cpp
@@ -6,17 +6,18 @@ class Solution {
     	int e = n-1;
     	int ans = INT_MAX;
     	while(s<=e){
-    		int mid = s + (e-s)/2;
-    
-    		// left sorted half
-    		if(arr[s] <= arr[mid]){
-    			ans = min(ans, arr[s]);
-    			s = mid + 1;
-    		}else{
-    			ans = min(ans, arr[mid]);
-    			e = mid - 1;
-    		}
+    	    int mid = s + (e-s)/2;
+    	    
+    	   // left sorted array
+    	   if(arr[s] <= arr[mid]){
+    	       ans = min(ans, arr[s]);
+    	       s = mid + 1;
+    	   }else{
+    	       ans = min(ans, arr[mid]);
+    	       e = mid - 1;
+    	   }
     	}
+    	
     	return ans;
     }
 };
